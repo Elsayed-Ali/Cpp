64ac151aa401bfb4f32c50cf5d3b4c37cb291dec
Alpha <foronlytyping@gmail.com>
2023-08-21 17:31:30 +0300

    This program to find the factorial of a number using a function

---
+++ Chapter_05/Factorial_function/Factorial_function/Source.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+
+int Factorial_function(int num)
+{
+	long long Factorial = 1;
+
+	for (int count = num; count >= 1; count--)
+	{
+		Factorial *= count;
+	}
+
+
+	return Factorial;
+
+}
+
+
+
+
+int main()
+{
+
+	cout << Factorial_function(10);
+	cout << endl;
+
+	return 0;
+}
