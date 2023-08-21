e9790428fdbe6d9117e4e357b18cca09e9233ae3
Alpha <foronlytyping@gmail.com>
2023-08-21 17:48:50 +0300

    array_parameter

---
+++ Chapter_05/Parameter_as_array/Parameter_as_array/Source.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+
+void Array_function(int array[], int array_size)
+{
+	int  Total_num = 0;
+
+	for (int index = 0; index < array_size; index++)
+	{
+		Total_num += array[index];
+	}
+
+	cout <<"Total sum " << Total_num;
+
+
+}
+
+
+
+
+int main()
+{
+	int array[] = { 10,20,30,40,50,60,70,80,90,100 };
+	int array_size = sizeof(array) / sizeof(array[0]);
+
+	Array_function(array, array_size);
+	cout << endl;
+
+	return 0;
+}
